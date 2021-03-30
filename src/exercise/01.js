a8e0ea1caf2648f993cc3fb87a81d2c660aa0752
amanda0617 <amanda21limah@gmail.com>
2021-03-30 12:36:02 +0000

    (23/03) Exercícios 01 a 04

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,27 +3,38 @@
 
 import * as React from 'react'
 
-function Greeting() {
+//recebe o initialName como um props ...props
+function Greeting({initialName = ''}) {
   // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+  // retorna um vetor, onde o primeiro elemento é a variável e o segundo é uma função que modifica o primeiro
+  // React.usestate('valor inicial da variável')
+  const [name, setName] = React.useState(initialName)
+  //let name = ''
 
+  //quem muda a variável de estado é a função HandleChange
+  //ela é acionada quando o conteúdo do input é alterado
   function handleChange(event) {
     // 🐨 update the name here based on event.target.value
+    // não pode atribuir valor a variável estado de forma direta, para isso usamos a função setName
+    //name = event.target.value
+    setName(event.target.value)
   }
 
+    //value={name} faz com que o input se inicie com o valor inicial da variável  
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input onChange={handleChange} id="name" value={name} /> 
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
   )
 }
 
+//initialName="Sicrano" é um parâmetro do Greeting
 function App() {
-  return <Greeting />
+  return <Greeting initialName="Sicrano" />
 }
 
 export default App
--- src/exercise/02.js
+++ src/exercise/02.js
@@ -4,28 +4,53 @@
 import * as React from 'react'
 
 function Greeting({initialName = ''}) {
-  // 🐨 initialize the state to the value from localStorage
+  // 🐨 initialize the state to the value from localStorage (um cookie usado para guardar alguma informação)
   // 💰 window.localStorage.getItem('name') || initialName
-  const [name, setName] = React.useState(initialName)
+  // ele recarrega aqui primeiro ( o estado initialName, que é o valor do localStorage guardado na memória)
+  // LAZY INIITIALIZER: inicializador "Preguiçoso"
+  // Quando o useState recebe uma função em vez de um valor como estado inicial,
+  // essa função é executada apenas durante a fase mount do componente, sem se
+  // repetir na fase update
+  const [name, setName] = React.useState(() => window.localStorage.getItem('name') || initialName)
+  const [count, setCount] = React.useState(0)
+  const [nameUC, setNameUC] = React.useState(() => window.localStorage.getItem('nameUC') || initialName)
 
   // 🐨 Here's where you'll use `React.useEffect`.
   // The callback should set the `name` in localStorage.
   // 💰 window.localStorage.setItem('name', name)
-
+  // a ideia desse exercicio é atualizar a variável estado e tbm o localStorage
   function handleChange(event) {
     setName(event.target.value)
   }
+
+  function handleClick(event) {
+      setNameUC(event.target.value.toUpperCase())
+  }
+
+  // Efeito colateral  a ser executado após o componente ter sido atualizado
+  React.useEffect(() => {
+      // O valor do localStorage será atualizado após a atualização do componente
+    window.localStorage.setItem('name', name)
+    window.localStorage.setItem('nameUC', nameUC)
+    setCount(count + 1)
+  }, [name, nameUC]) // [] é a lista de dependências, ou seja, esse useEffect é para ser chamado 
+  // apenas quando a variável name ou a variável nameUC sofrerem alterações
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input value={name} onChange={handleChange} id="name" />
+        <input value={name} onChange={handleChange} onClick={handleClick} id="name" />
       </form>
-      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+      {name ? <strong>Hello {name}, {nameUC}</strong> : 'Please type your name'}
+      <p>localStorage: {window.localStorage.getItem('name')} - {window.localStorage.getItem('nameUC')}</p>
+      <p>Contagem: {count}</p>
     </div>
   )
 }
-
+// a mudança do input provoca uma necessidade de mudar o estado e o localStorage sempre fica atualizado um 
+// passo a menos do que a variável estado
+// a atualização de tela ocorre antes da atualização do localStorage(sempre a versão anterior da variável)
+// tudo o que é armazenado no localStorage vai para o disco
 function App() {
   return <Greeting />
 }
